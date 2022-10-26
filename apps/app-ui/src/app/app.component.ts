82482dcd38760e78383ac76c7fb2865a5e169765
Nik Moiseienko <nik.moiseienko@gmail.com>
2022-10-26 22:56:10 +0300

    add app-ui

---
+++ apps/app-ui/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'homework-docusign-s3-root',
+  template: `
+    <h1>{{title}}</h1>
+  `,
+})
+export class AppComponent {
+  title = AppComponent.name;
+}
